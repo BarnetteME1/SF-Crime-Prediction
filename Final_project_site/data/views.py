abcf2757a907a2ea3e9b2c8629635a2869d63f84
Matthew Barnette <BarnetteME1@gmail.com>
2015-12-08 16:46:55 -0500

    fixing the site

--- Final_project_site/data/views.py
+++ Final_project_site/data/views.py
@@ -4,14 +4,11 @@ from django.shortcuts import render, render_to_response
 from django.views.generic import TemplateView
 
 
-def index_view(request):
-    context = {}
-    return render_to_response(template_name='base.html', context=context)
+class IndexView(TemplateView):
+    template_name = 'base.html'
 
-def map_view(request):
-    context = {}
-    return render_to_response(template_name='maps.html', context=context)
+class MapView(TemplateView):
+    template_name = 'maps.html'
 
-def graph_view(request):
-    context = {}
-    return render_to_response(template_name='graphs.html', context=context)
+class GraphView(TemplateView):
+    template_name = 'graphs.html'
